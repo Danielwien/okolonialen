1b3c02a481caed0af3090e1d740adcb344b057ff
Daniel Wien <danielwien@Daniels-MBP-2.dhcp.aplia.no>
2016-06-09 08:47:38 +0200

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,6 @@
+#!/Users/danielwien/okolonialen/venv/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'Django==1.11.dev20160608180924','django-admin.py'
+__requires__ = 'Django==1.11.dev20160608180924'
+__import__('pkg_resources').require('Django==1.11.dev20160608180924')
+__file__ = '/Users/danielwien/okolonialen/django/django/bin/django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
